a922c3694aee7059487afa2f661521136bc2cf8e
Nzagwaneza <anzagwaneza@gmail.com>
2023-06-17 16:35:34 -0700

    re-corrected

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,16 @@
 
 #include <stdio.h>
-
 /**
- * main - prints reversed lowercase alphabet 
- *
+ * main - prints reversed lowercase alphabet
  * Return: Always 0 (sucsess)
  */
 int main(void)
 {
-        char ch;
-
-        for (ch = 'z'; ch >= 'a'; ch--)
-        {
-                printf("%c", ch);
-        }
-
-        putchar('\n');
-
-        return 0;
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+printf("%c", ch);
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,23 +2,21 @@
 #include <stdio.h>
 
 /**
- * main - prints combination of single digit.
- *
- * Return: Always 0 (sucsess)
- */
+* main - prints combination of single digit.
+*
+* Return: Always 0 (sucsess)
+*/
 int main(void)
 {
-        int n;
-
-        for (n = 48; n < 58; n++)
-        {
-                putchar(n);
-		if(n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-        putchar('\n');
-        return 0;
+int n;
+for (n = 48; n < 58; n++)
+putchar(n);
+if (n != 57)
+{
+putchar (',')
+putchar (' ');
+}
+}
+putchar ('\n');
+return (0);
 }
