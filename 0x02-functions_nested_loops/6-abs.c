4cccd8c178bff23ac8c4f7ce750e5cd0ec00232d
Nzagwaneza <anzagwaneza@gmail.com>
2023-06-20 21:40:08 -0700

    nested functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*computes the absolute value of an integer.
+*return 0
+*/
+
+int _abs(int n)
+{
+    if (n >= 0)
+    {        
+    return (n);
+    }
+    else
+    return (-n);
+}
+
