3c5ace457e94483cd8309fa79bafe827fc9b83d9
Nzagwaneza <anzagwaneza@gmail.com>
2023-06-30 00:09:16 -0700

    re-collection

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,11 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistdio.h>
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, constchar *src, int n);
+char *_strncpy(char *dest, const char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
