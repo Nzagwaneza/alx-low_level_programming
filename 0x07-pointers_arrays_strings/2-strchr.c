48a3a4a05075759c0170497449a74444a52e1cc1
Nzagwaneza <anzagwaneza@gmail.com>
2023-07-06 01:05:57 -0700

    task :2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,19 +2,19 @@
 
 /**
  * _strchr - Searches for a character in a string
- * @str: Input string
- * @chr: Character to search for
+ * @s: Input string
+ * @c: Character to search for
  *
  * Return: Pointer to the first occurrence of the character in the string
  *         or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-int t = 0;
-for (; s[t] != '\0'; t++)
+int i = 0;
+for (; s[t] != '\0'; i++)
 {
-if (s[t] == c)
-return (&s[t]);
+if (s[i] == c)
+return (&s[i]);
 }
 return (NULL);
 }
