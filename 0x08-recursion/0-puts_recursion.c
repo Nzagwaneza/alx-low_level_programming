c5bb3bf04fe8960453d6f135faf95c1171283dd3
Nzagwaneza <anzagwaneza@gmail.com>
2023-07-07 15:50:24 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,12 +12,12 @@ void _puts_recursion(char *s)
 
 {
 
-if (*s !== '\0')
+if (*s == '\0')
 {
-_putchar('\n');
+putchar('\n');
 return;
 }
-_print_rev_recursion(s + 1);
-_putchar(*s);
+putchar(*s);
+_puts_recursion(s + 1);
 
 }
