0c7380669457a0cb4a32649ae85b28c50a07cc5b
Nzagwaneza <anzagwaneza@gmail.com>
2023-07-07 14:03:08 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts_recursion: prints a string, followed by a new line
+*@s: string to reverse
+*
+* Return: nothing
+*/
+
+void _puts_recursion(char *s);
+
+{
+if(* !s= '\0')
+{
+_putchar('\n')
+return;
+}
+_print_rev_recursion(s+1);
+_puchar(*s);
+}
+}
