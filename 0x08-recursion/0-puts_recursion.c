eec70c460b90da052e646c06d9e9067cbf63da4b
Nzagwaneza <anzagwaneza@gmail.com>
2023-07-07 16:08:02 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,23 +1,19 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
-*_puts_recursion: prints a string, followed by a new line
-*@s: string to reverse
-*
-* Return: nothing
-*/
-
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to reverse
+ *
+ * Return: nothing
+ */
 void _puts_recursion(char *s)
-
 {
-
-if (*s == '\0')
-{
-putchar('\n');
-return;
-}
-putchar(*s);
-_puts_recursion(s + 1);
-
+    if (*s == '\0')
+    {
+        putchar('\n');
+        return;
+    }
+    putchar(*s);
+    _puts_recursion(s + 1);
 }
