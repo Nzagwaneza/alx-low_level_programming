4a2b9bf1a42eeb0dee6abf119c0678407b79d3fd
Nzagwaneza <anzagwaneza@gmail.com>
2023-07-07 18:35:09 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-pow_recursion.c -o 4-pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,7 +6,7 @@
 * @x: number to raise
 * @y: power
 *
-* return: The result of x raised to the power of y.
+* Return: The result of x raised to the power of y.
 */
 
 int _pow_recursion(int x, int y)
