34a582efce83e86dd3b9519e43bb33aff49695a4
Nzagwaneza <anzagwaneza@gmail.com>
2023-07-07 18:11:27 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-pow_recursion.c -o 4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _pow_recursion - returns the value of x raised to the power of y
+* @x: A number to raise
+* @y: power
+*
+*return 1
+*/
+
+int _pow_recursion(int x, int y)
+
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+
+return (x * _pow_recursion(x, y - 1));
+}
