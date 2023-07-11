eb091ad15786972330f4c3123f93d066c3f50af0
Nzagwaneza <anzagwaneza@gmail.com>
2023-07-11 16:06:46 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,46 +1,41 @@
-#include <stdlib.h>
-#include "holberton.h"
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * argstostr - Concatenates all program arguments into a single string
- * @ac: Number of arguments
- * @av: Array of arguments
- *
- * Return: Pointer to the concatenated string (Success), NULL (Error)
+ * argstostr - Concatenates an array of strings into a single string
+ * @ac: The number of strings in the array
+ * @av: The array of strings
+ * Return: A pointer to the concatenated string, or NULL on failure
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+int i, j, total_length = 0, current_position = 0;
+char *str;
 
-	if (ac <= 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	len = 0;
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+total_length++;
+}
+total_length += ac;
 
-	str = malloc(sizeof(char) * (len + 1));
+str = malloc(sizeof(char) * (total_length + 1));
+if (str == NULL)
+return (NULL);
 
-	if (str == NULL)
-		return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+str[current_position] = av[i][j];
+current_position++;
+}
+str[current_position++] = '\n';
+}
+str[current_position] = '\0';
 
-	k = 0;
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k++] = av[i][j];
-		}
-		str[k++] = '\n';
-	}
-
-	str[k] = '\0';
-
-	return (str);
+return (str);
 }
