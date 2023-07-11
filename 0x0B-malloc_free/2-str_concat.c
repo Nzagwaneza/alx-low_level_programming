300d1b394012c2273eeebb0125f1fd3aec28f598
Nzagwaneza <anzagwaneza@gmail.com>
2023-07-11 15:01:25 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,28 +1,45 @@
 #include <stdlib.h>
-#include <string.h>
 #include "holberton.h"
 
 /**
- * str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
+ * alloc_grid - Creates a two-dimensional array of integers.
+ * @width: Width of the matrix.
+ * @height: Height of the matrix.
  *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * Return: Pointer to the created matrix (Success), or NULL (Error)
  */
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-	char *s3;
-	size_t len1 = s1 ? strlen(s1) : 0;
-	size_t len2 = s2 ? strlen(s2) : 0;
+	int **array;
+	int i, j;
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	if (s1)
-		strcpy(s3, s1);
-	if (s2)
-		strcat(s3, s2);
+	array = (int **)malloc(sizeof(int *) * height);
 
-	return (s3);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			free(array);
+			for (j = 0; j < i; j++)
+				free(array[j]);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+
+	return (array);
 }
